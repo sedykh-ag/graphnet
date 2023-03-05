130bf21f29ec7445a1f032bf345c7723d1aec2c4
Alexander Sedykh <sedykh.ag@phystech.edu>
2023-03-06 02:18:02 +0300

    added poisson script

---
+++ data/poisson.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import ufl
+from dolfinx import fem, io, mesh, plot
+from ufl import ds, dx, grad, inner
+
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+
+msh = mesh.create_rectangle(comm=MPI.COMM_WORLD,
+                            points=((0.0, 0.0), (1.0, 1.0)), n=(8, 8),
+                            cell_type=mesh.CellType.triangle,)
+V = fem.FunctionSpace(msh, ("Lagrange", 1))
+
+facets = mesh.locate_entities_boundary(msh, dim=(msh.topology.dim - 1),
+                                       marker=lambda x: np.isclose(x[0], 0.0) | \
+                                                        np.isclose(x[0], 1.0) | \
+                                                        np.isclose(x[1], 0.0) | \
+                                                        np.isclose(x[1], 1.0))
+
+dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
+bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
+
+# Define variational problem
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+x = ufl.SpatialCoordinate(msh)
+f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+a = inner(grad(u), grad(v)) * dx
+L = inner(f, v) * dx
+
+# Solve
+problem = fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+with io.XDMFFile(msh.comm, "out_poisson/poisson.xdmf", "w") as file:
+    file.write_mesh(msh)
+    file.write_function(uh)
+
+# Display solution
+try:
+    import pyvista
+    cells, types, x = plot.create_vtk_mesh(V)
+    grid = pyvista.UnstructuredGrid(cells, types, x)
+    grid.point_data["u"] = uh.x.array.real
+    grid.set_active_scalars("u")
+    plotter = pyvista.Plotter()
+    plotter.add_mesh(grid, show_edges=True)
+    warped = grid.warp_by_scalar()
+    plotter.add_mesh(warped)
+    if pyvista.OFF_SCREEN:
+        pyvista.start_xvfb(wait=0.1)
+        plotter.screenshot("uh_poisson.png")
+    else:
+        plotter.show()
+
+except ModuleNotFoundError:
+    print("'pyvista' is required to visualise the solution")
+    print("Install 'pyvista' with pip: 'python3 -m pip install pyvista'")
